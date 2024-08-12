6628a794dfa96130db823d89894dc3426ad8e858
Silvanos Eric <ericoderito@gmail.com>
2024-08-12 12:32:36 +0300

    feat(ui): update components with improved UI elements
    - Updated About component with enhanced layout including a heading, paragraph, and image.
    - Revised Home component to display a styled heading with dynamic content.
    - Modified NavBar component to include navigation links for 'Home' and 'About' sections.
    
    These changes enhance the user interface and improve navigation within the application.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <divk id="about">
+      <h2>About Me</h2>
+      <p>Let's ship stuff!</p>
+      <img src={image} alt="I made this" />
+    </divk>
+  );
 }
 
 export default About;
